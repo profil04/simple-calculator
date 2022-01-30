025766ad4a41d2d2f967d84aaf972c67f6028465
profil04 <prorok.filip@hotmail.com>
2022-01-30 20:30:30 +0100

    working calculator without history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,182 @@
+import React from 'react';
 import './App.css';
 import Calculator from './Calculator';
 
-function App() {
-  return(
-    <Calculator/>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [{
+        operations: Array(5).fill(null)
+      }],
+      firstIsNext: true,
+      firstParameter: "",
+      secondParameter: "",
+      operationSign: "",
+      result: 0,
+      stepNumber: 0,
+    }
+  }
+
+  handleClick(i) {
+    const firstParameter = this.state.firstParameter;
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const operations = current.operations.slice();
+
+    if (i >= 0 && i <= 9) {
+      this.setState({
+        firstParameter: firstParameter + String(i)
+      })
+    } else {
+      switch (i) {
+        case 10:
+          operations[0] = this.state.firstParameter;
+          if (this.state.firstIsNext) {
+            this.setState({
+              history: history.concat([{
+                operations: operations
+              }]),
+              firstIsNext: false,
+              operationSign: '+'
+            })
+          }
+          break;
+        case 11:
+          operations[0] = this.state.firstParameter;
+          if (this.state.firstIsNext) {
+            this.setState({
+              history: history.concat([{
+                operations: operations
+              }]),
+              firstIsNext: false,
+              operationSign: '-'
+            })
+          }
+          break;
+        case 12:
+          operations[0] = this.state.firstParameter;
+          if (this.state.firstIsNext) {
+            this.setState({
+              history: history.concat([{
+                operations: operations
+              }]),
+              firstIsNext: false,
+              operationSign: '*'
+            })
+          }
+          break;
+        case 13:
+          operations[0] = this.state.firstParameter;
+          if (this.state.firstIsNext) {
+            this.setState({
+              history: history.concat([{
+                operations: operations
+              }]),
+              firstIsNext: false,
+              operationSign: '/'
+            })
+          }
+          break;
+        case 14:
+          operations[3] = this.state.firstParameter;
+          switch (this.state.operationSign) {
+            case '+':
+              operations[0] = this.state.firstParameter;
+              operations[1] = this.state.operationSign;
+              operations[2] = this.state.secondParameter;
+              this.setState({
+                result: Number(this.state.firstParameter) + Number(this.state.secondParameter),
+                history: history.concat([{
+                  operations: operations
+                }]),
+              })
+              operations[4] = this.state.result;
+              break;
+            case '-':
+              operations[0] = this.state.firstParameter;
+              operations[1] = this.state.operationSign;
+              operations[2] = this.state.secondParameter;
+              this.setState({
+                result: Number(this.state.firstParameter) - Number(this.state.secondParameter),
+                history: history.concat([{
+                  operations: operations
+                }]),
+              })
+              operations[4] = this.state.result;
+              break;
+            case '*':
+              operations[0] = this.state.firstParameter;
+              operations[1] = this.state.operationSign;
+              operations[2] = this.state.secondParameter;
+              this.setState({
+                result: Number(this.state.firstParameter) * Number(this.state.secondParameter),
+                history: history.concat([{
+                  operations: operations
+                }]),
+              })
+              operations[4] = this.state.result;
+              break;
+            case '/':
+              operations[0] = this.state.firstParameter;
+              operations[1] = this.state.operationSign;
+              operations[2] = this.state.secondParameter;
+              this.setState({
+                result: Number(this.state.firstParameter) / Number(this.state.secondParameter),
+                history: history.concat([{
+                  operations: operations
+                }]),
+              })
+              operations[4] = this.state.result;
+              break;
+          }
+          break;
+        default:
+        // code block
+      }
+    }
+  }
+
+  jumpTo(step) {
+    const firstParameter = this.state.firstParameter;
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const operations = current.operations.slice();
+    this.setState({
+      stepNumber: step,
+      firstIsNext: true,
+      firstParameter: operations[0],
+      operationSign: operations[1],
+      secondParameter: operations[2],
+      result: operations[4],
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+
+    const actions = history.map((step, action) => {
+      const desc = action ?
+        'Go to action #' + action :
+        'Go to game start';
+      return (
+        <li key={action}>
+          <button onClick={() => this.jumpTo(action)}>{desc}</button>
+        </li>
+      );
+    });
+
+    return (
+      <div>
+      <Calculator
+        onClick={(i) => this.handleClick(i)}
+        result={this.state.result}
+      />
+      <ol>{actions}</ol>
+      </div>
+    );
+  }
 }
 
-export default App;
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
 import CalculatorButton from './CalculatorButton';
+import './App.css';
 
 export default class Calculator extends Component {
+
+  
+  renderCalculatorButton(i) {
+    const valueTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '*', '/', '=' ]
+    return(
+      <CalculatorButton
+        value = {valueTable[i]}
+        onClick ={() => this.props.onClick(i)}
+      />
+    );
+  }
+
   render() {
     return (
-    <div>
-        <div className='display'></div>
+    <div className='calculatorContainer'>
+        <div className='display'>{this.props.result}</div>
         <div className='history'></div>
-        <CalculatorButton sign = '1'></CalculatorButton>
-        <CalculatorButton sign = '2'></CalculatorButton>
-        <CalculatorButton sign = '3'></CalculatorButton>
-        <CalculatorButton sign = '4'></CalculatorButton>
-        <CalculatorButton sign = '5'></CalculatorButton>
-        <CalculatorButton sign = '6'></CalculatorButton>
-        <CalculatorButton sign = '7'></CalculatorButton>
-        <CalculatorButton sign = '8'></CalculatorButton>
-        <CalculatorButton sign = '9'></CalculatorButton>
-        <CalculatorButton sign = '0'></CalculatorButton>
-        <CalculatorButton sign = '+'></CalculatorButton>
-        <CalculatorButton sign = '-'></CalculatorButton>
-        <CalculatorButton sign = '*'></CalculatorButton>
-        <CalculatorButton sign = '/'></CalculatorButton>
-        <CalculatorButton sign = '='></CalculatorButton>
+        {this.renderCalculatorButton(1)}
+        {this.renderCalculatorButton(2)}
+        {this.renderCalculatorButton(3)}
+        {this.renderCalculatorButton(4)}
+        {this.renderCalculatorButton(5)}
+        {this.renderCalculatorButton(6)}
+        {this.renderCalculatorButton(7)}
+        {this.renderCalculatorButton(8)}
+        {this.renderCalculatorButton(9)}
+        {this.renderCalculatorButton(0)}
+        {this.renderCalculatorButton(10)}
+        {this.renderCalculatorButton(11)}
+        {this.renderCalculatorButton(12)}
+        {this.renderCalculatorButton(13)}
+        {this.renderCalculatorButton(14)}
     </div>
     );
   }
